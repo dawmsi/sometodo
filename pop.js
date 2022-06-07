12362065ab1749eaf3dede619d1bb7674638436c
Denys Shapoval <url.my.email@gmail.com>
2022-06-07 12:56:37 +0300

    add icons & pop window

---
+++ pop.js
@@ -0,0 +1,8 @@
+document.querySelector('#add')
+.addEventListener('click', togglePop)
+
+function togglePop(){
+    let pop = document.querySelector('#pop-adding')
+        pop.classList.toggle("hiding")
+        pop.classList.contains("hiding") ? pop.style.visibility = 'hidden' : pop.style.visibility = 'visible'
+}
