82b52ee7b061bfead8c88888d10a9f14bae25d3f
Denys Shapoval <url.my.email@gmail.com>
2022-07-11 12:55:43 +0300

    fix input color

--- modules/Editor.js
+++ modules/Editor.js
@@ -5,7 +5,7 @@ class Editor {
         btnShowEditor,
         wrapperEditor
     ) {
-        // inputColor.options.selectedIndex = 0
+        inputColor.options.selectedIndex = 0
         inputColor.style = `color: none`
         inputName.value = ''
         btnShowEditor.classList.toggle('cencelBtn')
@@ -29,6 +29,7 @@ class Editor {
         if (index || index === 0) {
             Taker.touchArray(projectsArr)
             inputName.value = Taker.someArray[index].someName
+            inputColor.value = Taker.someArray[index].someColor
             inputColor.style = `color:${Taker.someArray[index].someColor}`
             btnSubmit.innerHTML = `
             <button onclick="${Project.name}.editEl(${index})">
