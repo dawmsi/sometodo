b59dc4e0c55b8ed62a79bc2c60deab1af3c87cbf
Denys Shapoval <url.my.email@gmail.com>
2022-07-27 20:44:57 +0300

    Editor better readability + optimiztion

--- modules/Editor.js
+++ modules/Editor.js
@@ -20,51 +20,54 @@ class Editor {
             inputHTMLObj.wrapperEditor,
             inputHTMLObj.btnShowEditor
         )
+        //* default
+        if (inputHTMLObj.inputSelect) {
+            inputHTMLObj.inputSelect.style.color = inputHTMLObj.inputSelect.options[inputHTMLObj.inputSelect.options.selectedIndex].style.color
+            inputHTMLObj.inputSelect.addEventListener("change", () => {
+                inputHTMLObj.inputSelect.style.color = inputHTMLObj.inputSelect.options[inputHTMLObj.inputSelect.options.selectedIndex].style.color
+            })
+        }
+        //* for edit
         if (index || index === 0) {
-            inputHTMLObj.inputName.value = inputArray[index].someName
-            if (inputHTMLObj.inputSelect) {
+            if (inputHTMLObj.inputSelect && inputHTMLObj.inputName && inputHTMLObj.btnSubmit) {
+                inputHTMLObj.inputName.value = inputArray[index].someName
                 inputHTMLObj.inputSelect.selectedIndex = inputArray[index].someSelect
                 if (newParent.name === 'Task') {
                     inputHTMLObj.inputSelect.style.color = colorsProjects[projectsArr[inputArray[index].someSelect].someSelect]
-                } else {
-                    inputHTMLObj.inputSelect.style.color = inputArray[index].someSelect[1]
+                    inputHTMLObj.btnSubmit.innerHTML = `
+                        <button onclick="${newParent.name}.edit(${newParent.nameArray},${index})">Save</button>
+                        `
                 }
-                inputHTMLObj.inputSelect.addEventListener("change", () => {
-                    inputHTMLObj.inputSelect.style.color = inputHTMLObj.inputSelect.options[inputHTMLObj.inputSelect.options.selectedIndex].style.color
-                })
+                if (newParent.name === 'Project' || newParent.name === 'Mark') {
+                    inputHTMLObj.btnSubmit.innerHTML = `
+                    <button onclick="${newParent.name}.edit(${newParent.nameArray},${index})">
+                        <i class="fa fa-pencil"></i>
+                        </button>
+                    `
+                }
+                if (newParent.name === 'Project') {
+                    inputHTMLObj.inputSelect.style.color = colorsProjects[inputArray[index].someSelect]
+                }
+                if (newParent.name === 'Mark') {
+                    inputHTMLObj.inputSelect.style.color = colorsMarks[inputArray[index].someSelect]
+                }
             }
             if (inputHTMLObj.description) {
                 inputHTMLObj.description.value = inputArray[index].someDescription
             }
-            if (newParent.name === 'Task') {
-                inputHTMLObj.btnSubmit.innerHTML = `
-                <button onclick="${newParent.name}.edit(${newParent.nameArray},${index})">Save</button>
-                `
-            } else {
-                inputHTMLObj.btnSubmit.innerHTML = `
-                <button onclick="${newParent.name}.edit(${newParent.nameArray},${index})">
-                    <i class="fa fa-pencil"></i>
-                    </button>
-                `
-            }
         }
+        //* for add
         else {
-            if (inputHTMLObj.inputSelect) {
-                inputHTMLObj.inputSelect.style.color = inputHTMLObj.inputSelect.options[inputHTMLObj.inputSelect.options.selectedIndex].style.color
-                inputHTMLObj.inputSelect.addEventListener("change", () => {
-                    inputHTMLObj.inputSelect.style.color = inputHTMLObj.inputSelect.options[inputHTMLObj.inputSelect.options.selectedIndex].style.color
-                })
-            }
             if (newParent.name === 'Task') {
                 inputHTMLObj.btnSubmit.innerHTML = `
                 <button onclick="${newParent.name}.add(${newParent.nameArray})">Add</button>
                 `
-            } else {
+            }
+            if (newParent.name === 'Project' || newParent.name === 'Mark') {
                 inputHTMLObj.btnSubmit.innerHTML = `
                 <button onclick="${newParent.name}.add(${newParent.nameArray})">
-                    <i class="fa fa-check"></i>
-                    </button>
-                `
+                <i class="fa fa-check"></i>
+                </button>`
             }
         }
     }
