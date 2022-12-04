a63eb2eff9016505c9c975c6c99ad0a8b9be739c
Denys Shapoval <96021986+dawmsi@users.noreply.github.com>
2022-12-04 16:11:14 +0200

    close develop

--- structure/aside/Aside.js
+++ structure/aside/Aside.js
@@ -1,14 +1,17 @@
 class Aside {
-    static aside = document.querySelector("#aside");
-    static eye = document.querySelector("#eye");
+    static aside = document.querySelector('#aside')
+    static eye = document.querySelector('#eye')
+    static main = document.querySelector('#main')
+
     static hideshow() {
-        this.aside.classList.toggle("hiden");
-        if (aside.classList.contains("hiden")) {
-            this.eye.classList.remove("fa-eye-slash");
-            this.eye.classList.add("fa-eye");
+        this.aside.classList.toggle('hiden')
+        this.main.classList.toggle('hideWhenMobile')
+        if (aside.classList.contains('hiden')) {
+            this.eye.classList.remove('fa-eye-slash')
+            this.eye.classList.add('fa-eye')
         } else {
-            this.eye.classList.remove("fa-eye");
-            this.eye.classList.add("fa-eye-slash");
+            this.eye.classList.remove('fa-eye')
+            this.eye.classList.add('fa-eye-slash')
         }
     }
 }
